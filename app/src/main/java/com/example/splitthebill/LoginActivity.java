ee41f70e1c807553a05036b1a0f45981bcac22d7
Cyril Junior Dim <Cyril Junior Dim>
2021-01-28 23:01:27 +0100

    login screen and activity configured... must now link to server

--- app/src/main/java/com/example/splitthebill/LoginActivity.java
+++ app/src/main/java/com/example/splitthebill/LoginActivity.java
@@ -2,13 +2,62 @@ package com.example.splitthebill;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText username, password;
+    TextView signUp;
+    TextView forgotPwd;
+    ProgressBar progressBar;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        progressBar = findViewById(R.id.loginProgressBar);
+        username = findViewById(R.id.emailAddress);
+        password = findViewById(R.id.password);
+        signUp = findViewById(R.id.goToSignUp);
+        forgotPwd = findViewById(R.id.forgotPassword);
+        login = findViewById(R.id.login);
+
+        progressBar.setVisibility(View.GONE);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = username.getText().toString().trim();
+                String pwd = password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    username.setError("Username or Email is required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(pwd)){
+                    password.setError("Password is required");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+            }
+        });
+
     }
 }
